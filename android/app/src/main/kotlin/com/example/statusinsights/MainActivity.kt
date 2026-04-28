409f722873b779086a676b9fcc1e6ad4c64a4e63
Wenxuan Shen <bushigemen114@gmail.com>
2026-04-28 19:52:36 +0800

    chore: 修改包名和软件名

--- android/app/src/main/kotlin/com/example/statusinsights/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.statusinsights
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/helloswx/statusinsights/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.helloswx.statusinsights
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()package com.helloswx.statusinsights
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
